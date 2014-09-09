6a23eb927ee42313bea880ddfcc9c9759b641c70
Grégory DAVID <groolot@groolot.net>
2014-09-09 09:08:46 +0200

    Update AccesDB.py
    Ajout des informations d'en-tête de fichier

--- AccesDB.py
+++ AccesDB.py
@@ -1,3 +1,24 @@
+'''
+Ce fichier est un fichier d'exemple d'utilisation des différentes classes disponibles.
+En tant qu'exemple, il peut contenir des erreurs, ou ne pas convenir aux objectifs que vous vous définirez.
+Prenez soin de bien lire le code source, le comprendre et vous en inspirer au besoin.
+
+    AccesDB.py, exemple d'utilisation
+    Copyright (C) 2012-2014  Grégory DAVID, Géraldine TAYSSE
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import sqlite3
 from CreationDB import GestionBD
 from PlayList import PlayList
